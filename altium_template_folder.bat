e9e0534b7a2a8a692fdc7b38d8ded488a45ce1bb
SreejitS <iamsreejits@gmail.com>
2018-06-05 12:01:17 +0530

    added template folder;completed fedevels tips

--- altium_template_folder.bat
+++ altium_template_folder.bat
@@ -2,3 +2,5 @@ md "template"
 cd template/
 md "integrated_library" "datasheets" "design_files" "manufacturing"
 echo "Write what this PCB is intended for" > "PCB_description.txt"
+cd integrated_library/
+md "3D_models"
